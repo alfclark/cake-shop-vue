339be3bdd3945bba3a4fed2bbd28d36cf8d424ca
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-03-26 15:32:00 -0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+      <a class="navbar-brand"
+        ><img class="logo" src="@/assets/logo.png" alt=""
+      /></a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link"><router-link to="/">Home</router-link></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link"><router-link to="/about">About</router-link></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link"
+              ><router-link to="/about">Catalog</router-link></a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link"
+              ><router-link to="/about">Build Your Cake</router-link></a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link"
+              ><router-link to="/about"
+                ><router-link to="/about"
+                  ><i
+                    class="fa-solid fa-cart-shopping"
+                  ></i></router-link></router-link
+            ></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap");
+:root {
+  --purple: #6242c1bb;
+  --beige: #fcf8f8;
+  --yellow: #f2b233;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.navbar {
+  padding: 0 3rem 0 3rem;
+  height: 10vh;
+}
+.logo {
+  width: 4rem;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
+  font-size: 1;
+}
+a:hover {
+  color: var(--purple);
+  transition: 0.4s;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <h1>Build the Cake of your dreams</h1>
+    <p>Use our new tool to build your own cake.</p>
+    <button class="build-btn">
+      <router-link class="btn-text" to="/about">Build Cake</router-link>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@font-face {
+  font-family: Junitta;
+  src: url("@/assets/Junitta-BW55G.otf");
+}
+.hello {
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.hello h1 {
+  font-family: Junitta;
+  font-size: 6rem;
+  text-align: center;
+}
+.build-btn {
+  background-color: var(--purple);
+  font-size: 1.5rem;
+  border: none;
+  color: var(--beige);
+  padding: 0.5rem 1.5rem;
+  cursor: pointer;
+  border-radius: 2rem;
+}
+.btn-text {
+  color: white;
+}
+
+.build-btn:hover {
+  color: var(--beige);
+  background-color: #000000;
+  transition: 0.6s;
+}
+@media screen and (max-width: 450px) {
+  .hello h1 {
+    font-size: 3rem;
+  }
+  .build-btn {
+    font-size: 1rem;
+  }
+  .build-title {
+    font-size: 3rem;
+  }
+  .showcase {
+    align-items: center;
+  }
+}
+</style>
