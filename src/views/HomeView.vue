09ef824ba9c274a680c9e9a0511380077b68c7f2
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-03-28 16:15:25 -0700

    Routing to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    <LandingScreen />
+    <FooterComp />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import LandingScreen from "@/components/LandingScreen.vue";
+import FooterComp from "@/components/FooterComp.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    LandingScreen,
+    FooterComp,
   },
 };
 </script>
