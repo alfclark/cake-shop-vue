faa6276e0a2f6863642f17dcc316147054fe05bc
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-03-29 13:21:05 -0700

    Catalog element

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <LandingScreen />
+    <CatalogView />
     <FooterComp />
   </div>
 </template>
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import LandingScreen from "@/components/LandingScreen.vue";
 import FooterComp from "@/components/FooterComp.vue";
+import CatalogView from "@/components/CatalogView.vue";
 
 export default {
   name: "HomeView",
   components: {
     LandingScreen,
     FooterComp,
+    CatalogView,
   },
 };
 </script>
